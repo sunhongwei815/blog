0c0020c12aceb7494d7570f2b76471f3bacdae64
Derrick <sunhongwei658@163.com>
2020-02-20 02:10:57 +0800

    update_UserMapper and Controller

--- src/main/java/com/example/blog/bean/UserBean.java
+++ src/main/java/com/example/blog/bean/UserBean.java
@@ -21,7 +21,6 @@ public class UserBean {
         this.role = role;
     }
 
-
     public int getId() {
         return id;
     }
--- src/main/java/com/example/blog/controller/UserController.java
+++ src/main/java/com/example/blog/controller/UserController.java
@@ -3,11 +3,17 @@ package com.example.blog.controller;
 import com.example.blog.bean.UserBean;
 import com.example.blog.service.UserService;
 import com.example.blog.serviceImpl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author:Sun Hongwei
@@ -18,11 +24,92 @@ import java.util.List;
 @Controller
 public class UserController {
 
-    private UserService userService;
+    @Autowired
+    UserService userService;
 
-    @RequestMapping("/getuserlist")
-    public List<UserBean> getUserList()
-    {
-        return userService.queryAllUser();
+
+    /**
+     * 查询所有用户信息
+     * @param modelMap
+     * @return
+     */
+    @RequestMapping("/users")
+    public String showUsers(ModelMap modelMap){
+        List<UserBean> userList = userService.queryAllUser();
+        modelMap.addAttribute("userList",userList);
+        return "users";
     }
+
+    /**
+     * 新增用户
+     * remark：这里未做用户名是否重复校验
+     * @param userBean
+     * @return
+     */
+    @RequestMapping("/addUser")
+    @ResponseBody
+    public Map addUser(UserBean userBean){
+        int flag = userService.addUser(userBean);
+        Map<String,Object> map = new HashMap<String,Object>();
+        if(flag == 1){
+            map.put("msg","新增用户成功");
+            return map;
+        }else {
+            map.put("msg","新增用户失败");
+            return map;
+        }
+    }
+
+
+    /**
+     * 根据用户ID删除用户信息
+     * @param id
+     * @param modelMap
+     * @return
+     */
+    @RequestMapping(value = "/deleteUser+{id}")
+    public String dropUser(@PathVariable("id") String id, ModelMap modelMap){
+        int flag = userService.dropUser(id);
+        List<UserBean> userList = userService.queryAllUser();
+        modelMap.addAttribute("userList",userList);
+        if(flag == 1){
+            return "users";
+        }else {
+            return "error";
+        }
+    }
+
+    /**
+     * 根据用户ID查询用户信息
+     * @param id
+     * @return
+     */
+    @RequestMapping("/user+{id}")
+    public String queryUser(@PathVariable("id") String id,ModelMap modelMap){
+        UserBean userBean = userService.queryUserById(id);
+        modelMap.addAttribute("user",userBean);
+        return "userInfo";
+    }
+
+    /**
+     * 根据用户ID修改用户信息
+     * remark：这里未做用户名是否重复校验
+     * @param userBean
+     * @return
+     */
+    @RequestMapping("/modifyUser")
+    @ResponseBody
+    public Map modifyUser(UserBean userBean){
+        int flag = userService.modifyUser(userBean);
+        Map<String,Object> map = new HashMap<>();
+        if(flag == 1){
+            map.put("msg","修改用户信息成功");
+            return map;
+        }else {
+            map.put("msg","修改用户信息失败");
+            return map;
+        }
+    }
+
+
 }
